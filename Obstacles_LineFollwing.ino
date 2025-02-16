b5a0952f96fee45f738f785a55d4b50391d52517
Ng Kah Lok <ngkahlok0407@gmail.com>
2025-02-16 21:32:01 +0800

    Line following and obstacles avoidance
    16/2/2026 Latest code for line following and obstacles avoidance function
    - After detecting obstacles, the code has not polish/ change yet

---
+++ Obstacles_LineFollwing.ino
@@ -0,0 +1,260 @@
+#include <Servo.h>
+
+const int ENA = 7;
+const int IN1 = 6;
+const int IN2 = 5;
+const int IN3 = 4;
+const int IN4 = 3;
+const int ENB = 2;
+
+const int TRIG = 11;
+const int ECHO = 12;
+Servo myServo;
+
+
+const int IrLeftSensor = 8;
+const int IrRightSensor = 9;
+const int IrCenterSensor = 10;
+
+// PID constants
+float Kp = 1.06;   // Proportional gain
+float Ki = 0.0;   // Integral gain (set to 0 initially)
+float Kd = 0.53;   // Derivative gain
+
+// Variables for PID control
+float error = 0;              // Current error
+float lastError = 0;          // Previous error (for derivative)
+float integral = 0;           // Cumulative error (for integral)
+
+// Motor speed variables
+int baseSpeed = 80;          // Base speed of the motors
+int leftMotorSpeed;
+int rightMotorSpeed;
+
+long distance_L;
+long distance_R;
+
+int maxDistanceFromObstacles = 40; //ADDED THIS
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IrLeftSensor, INPUT);
+  pinMode(IrRightSensor, INPUT);
+  pinMode(IrCenterSensor, INPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  myServo.attach(13);
+  
+  delay(1000);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int left = digitalRead(IrLeftSensor);
+  int center = digitalRead(IrCenterSensor);
+  int right = digitalRead(IrRightSensor);
+
+  //ADDED THIS
+  long distanceFromObstacles = ultrasonicRead();
+
+  PID(left,center,right);
+
+  if(distanceFromObstacles > maxDistanceFromObstacles){
+    if (left == 0 && center == 1 && right == 0) {
+        moveForward(); // Robot is centered on the line
+    } else if (left == 1 && center == 1 && right == 0) {
+        turnLeft(); // Slightly right of the line
+    } else if (left == 0 && center == 1 && right == 1) {
+        turnRight(); // Slightly left of the line
+    } else if (left == 1 && center == 0 && right == 0) {
+        turnLeft(); // Far right
+    } else if (left == 0 && center == 0 && right == 1) {
+        turnRight(); // Far left
+    } else if (left == 0 && center == 0 && right == 0) {
+        reverse();
+    }
+  }
+  else{
+    checkSide();
+  }
+  
+}
+
+
+void PID(int left, int center, int right) {
+    // Calculate error based on sensor readings
+    if (left == 0 && center == 1 && right == 0) {
+        error = 0; // Robot is centered on the line
+    } else if (left == 1 && center == 1 && right == 0) {
+        error = 1; // Slightly right of the line
+    } else if (left == 0 && center == 1 && right == 1) {
+        error = -1; // Slightly left of the line
+    } else if (left == 1 && center == 0 && right == 0) {
+        error = 2; // Far right
+    } else if (left == 0 && center == 0 && right == 1) {
+        error = -2; // Far left
+    }
+
+    // PID calculations
+    float P = error;                        // Proportional term
+    integral += error;                      // Integral term
+    float I = integral;                     
+    float D = error - lastError;            // Derivative term
+    float correction = (Kp * P) + (Ki * I) + (Kd * D);
+
+    // Update last error for next iteration
+    lastError = error;
+
+    // Calculate motor speeds
+    leftMotorSpeed = baseSpeed - correction;
+    rightMotorSpeed = baseSpeed + correction;
+
+    // Constrain motor speeds to valid range (0-255)
+    leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
+    rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
+
+    delay(10); // Short delay to stabilize readings
+}
+
+void moveForward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  
+  analogWrite(ENA, rightMotorSpeed);
+  analogWrite(ENB, leftMotorSpeed);
+  delay(10);
+}
+
+void reverse() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
+  analogWrite(ENA, rightMotorSpeed);
+  analogWrite(ENB, leftMotorSpeed);
+  delay(10);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
+  analogWrite(ENA, rightMotorSpeed);
+  analogWrite(ENB, leftMotorSpeed);
+  delay(10);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  
+  analogWrite(ENA, rightMotorSpeed);
+  analogWrite(ENB, leftMotorSpeed);
+  delay(10);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  
+  delay(10);
+}
+
+//**********************Ultrasonic_read****************************
+long ultrasonicRead(){
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  long time = pulseIn (ECHO, HIGH);
+  return time * 0.0343 / 2;
+}
+
+void compareDistance(){
+  if(distance_L > distance_R){
+    turnLeft();
+    delay(500);
+    moveForward();
+    delay(600);
+    turnRight();
+    delay(500);
+    moveForward();
+    delay(600);
+    turnRight();
+    delay(400);
+  }
+  else{
+    turnRight();
+    delay(500);
+    moveForward();
+    delay(600);
+    turnLeft();
+    delay(500);
+    moveForward();
+    delay(600);  
+    turnLeft();
+    delay(400);
+  }
+}
+void checkSide()
+{
+  stopMotors();
+  delay(100);
+
+ for (int angle = 70; angle <= 140; angle += 5)  {
+    // servoPulse(servo, angle);  
+    myServo.write(angle);
+  }
+  delay(1000);
+  distance_R = ultrasonicRead();
+  Serial.print("D R=");
+  Serial.println(distance_R);
+  delay(100);
+
+  for (int angle = 140; angle >= 0; angle -= 5)  {
+    // servoPulse(servo, angle); 
+    myServo.write(angle);
+  }
+  delay(1000);
+  distance_L = ultrasonicRead();
+  Serial.print("D L=");
+  Serial.println(distance_L);
+  delay(100);
+
+ for (int angle = 0; angle <= 70; angle += 5)  {
+    // servoPulse(servo, angle);  
+    myServo.write(angle);
+  }
+
+  delay(300);
+  compareDistance();
+}
+
+// void servoPulse (int pin, int angle){
+//   int pwm = (angle*11) + 500;      // Convert angle to microseconds
+//  digitalWrite(pin, HIGH);
+//  delayMicroseconds(pwm);
+//  digitalWrite(pin, LOW);
+//  delay(50); // Refresh cycle of servo
+// }
+
+
